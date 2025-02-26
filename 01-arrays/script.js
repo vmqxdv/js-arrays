0a244d50fa9dd53ee6b31dadd0ed77c86896d55d
vmqxdv <vquintilio@gmail.com>
2025-02-26 14:55:38 +0100

    01 N.8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -33,4 +33,4 @@ const lewisIndex = teachers.indexOf('Lewis');
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
