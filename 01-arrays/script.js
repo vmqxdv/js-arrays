cb43e5e81b6c90efbf670d971bea0652a4a3e6d1
vmqxdv <vquintilio@gmail.com>
2025-02-26 14:51:46 +0100

    01 N.6

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -25,6 +25,7 @@ const firstTeacher = teachers.shift();
 teachers.push('Vanessa');
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah');
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
