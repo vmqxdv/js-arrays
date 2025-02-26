0e8de1d13d366f0e51cd0cf21f594785a5d08bdd
vmqxdv <vquintilio@gmail.com>
2025-02-26 14:49:27 +0100

    01 N.3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,7 +16,7 @@ const fourthTeacher = teachers[3];
 teachers.splice(4, 1, 'Patrick');
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
